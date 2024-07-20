cf0725a7e162b1ffe843019713757452d0dac89b
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-07-20 11:44:17 +0700

    no 5 error result 1

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 
 import StudentView from '@/views/StudentView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -62,6 +63,18 @@ const router = createRouter({
       path:'/students',
       name:'student',
       component:StudentView
+    },
+    {
+      path:'/404/:resource',
+      name:'404-resource-view',
+      component: NotFoundView,
+      props:true
+    },
+
+    {
+      path:'/:catchAll(.*)',
+      name:'not-found',
+      component: NotFoundView
     }
   ]
 })

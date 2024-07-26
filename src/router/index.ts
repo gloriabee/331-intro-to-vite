d3b99107fcdf079c488e2d5f0e5bee3912284815
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-07-26 09:52:41 +0700

    network error view

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ import EventLayoutView from '@/views/event/LayoutView.vue'
 
 import StudentView from '@/views/StudentView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,9 +17,9 @@ const router = createRouter({
       path: '/',
       name: 'event-list-view',
       component: EventListView,
-      props:(route)=>({
+      props: (route) => ({
         page: parseInt(route.query.page?.toString() || '1'),
-        pageSize:Number(route.query.pageSize)
+        pageSize: Number(route.query.pageSize)
       })
     },
     {
@@ -27,26 +28,26 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component:EventLayoutView,
-      props:true,
-      children:[
+      component: EventLayoutView,
+      props: true,
+      children: [
         {
-          path:'',
-          name:'event-detail-view',
-          component:EventDetailView,
-          props:true
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
         },
         {
-          path:'register',
-          name:'event-register-view',
-          component:EventRegisterView,
-          props:true
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
         },
         {
-          path:'edit',
-          name:'event-edit-view',
-          component:EventEditView,
-          props:true
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
         }
       ]
     },
@@ -58,23 +59,28 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: AboutView
     },
-   
+
     {
-      path:'/students',
-      name:'student',
-      component:StudentView
+      path: '/students',
+      name: 'student',
+      component: StudentView
     },
     {
-      path:'/404/:resource',
-      name:'404-resource-view',
+      path: '/404/:resource',
+      name: '404-resource-view',
       component: NotFoundView,
-      props:true
+      props: true
     },
 
     {
-      path:'/:catchAll(.*)',
-      name:'not-found',
+      path: '/:catchAll(.*)',
+      name: 'not-found',
       component: NotFoundView
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
     }
   ]
 })
---
+++ src/views/NetworkErrorView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="networkError">
+    <h1>Uh-Oh!</h1>
+    <h3>
+      It looks like you're experiencing some network issues, please take a breadth and
+      <a href="#" @click="$router.go(-1)">Click here</a> to try again
+    </h3>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

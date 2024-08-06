c9692467ebbbfe948b581be8a18d8fe423638998
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-08-06 13:03:05 +0700

    edit event card with tailwind css

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import {type Event} from '@/types'
+import { type Event } from '@/types'
 defineProps<{
-    event: Event
+  event: Event
 }>()
 // const event=ref({
 //     id:5928101,
@@ -17,29 +17,27 @@ defineProps<{
 </script>
 
 <template>
-<RouterLink  class="event-link" :to="{name: 'event-detail-view',params:{id: event.id}}">
-    <div class="event-card">
-        <h2>{{ event.title }}</h2>
-        <span>@{{ event.time }} on {{ event.date }}</span>
+  <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
+    <div
+      class="cursor-pointer border border-gray-600 p-4 w-64 mb-6 hover:scale-101 hover:shadow-sp"
+    >
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
-</RouterLink>
+  </RouterLink>
 </template>
 
 <style scoped>
-.event-card{
-    padding: 20px;
-    width:250px;
-    cursor: pointer;
-    border: 1px solid #39495c;
-    margin-bottom: 18px;
-}
-.event-card:hover{
-    transform: scale(1.01);
-    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
-.event-link{
-    text-decoration: none;
-    color: #2c3e50;
+/* .event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+} */
+
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
-

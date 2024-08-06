ce74f2d8cbfca818880d11c831b6c0761ed7e244
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-08-06 13:56:21 +0700

    fix error of types

--- src/components/EventCategory.vue
+++ src/components/EventCategory.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
-import {ref} from 'vue'
-import Event from '@/types/Event'
+import { ref } from 'vue'
+import { type Event } from '@/types'
 defineProps<{
-    event: Event
+  event: Event
 }>()
 </script>
 
 <template>
-    <div class="details">
-            <span>Category Name: {{event.category}}</span> <br>
-            <span>Organizer: {{event.organizer}}</span>
-    </div>
+  <div class="details">
+    <span>Category Name: {{ event.category }}</span> <br />
+    <span>Organizer: {{ event.organizer }}</span>
+  </div>
 </template>
 
 <style scoped>
-    .details{
-        padding: 20px;
-        text-align: right;
-        font-size: 16px;
-        border: 1px solid #39495c;
-        margin-bottom: 18px;
-    }
-
+.details {
+  padding: 20px;
+  text-align: right;
+  font-size: 16px;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
 </style>
